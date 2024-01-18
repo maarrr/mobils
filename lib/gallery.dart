b4be2f37d13b38531d3210e330c53b2578563ab3
marfranco <mfranco@cactusiot.com>
2024-01-18 22:46:35 +0100

    upload and download images

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
@@ -5,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mobils/store.dart';
 import 'package:path/path.dart';
 
 
@@ -21,8 +23,11 @@ class GalleryScreen extends StatefulWidget {
 
 class _GalleryScreenState extends State<GalleryScreen> {
   String? imagePath;
-  // A reference to the Firebase Storage bucket
-  final storageRef = FirebaseStorage.instance.ref().child('photos');
+  // Declare a StreamController of List<String>
+  late StreamController<List<String>> imageUrlsController;
+
+// Declare a getter for the stream of image URLs
+  Stream<List<String>> get imageUrlsStream => imageUrlsController.stream;
 
   // A stream of the photos in the Firebase Storage bucket
   late Stream<List<Reference>> photosStream;
@@ -30,10 +35,17 @@ class _GalleryScreenState extends State<GalleryScreen> {
   firebase_storage.FirebaseStorage storage =
       firebase_storage.FirebaseStorage.instance;
 
+  final storageRef = FirebaseStorage.instance.ref();
+
+
+
+
   @override
   void initState() {
     super.initState();
-    photosStream = storageRef.listAll().asStream().map((result) => result.items);
+    imageUrlsController = StreamController<List<String>>.broadcast();
+    getAllImagesFromStorage();
+
   }
 
   @override
@@ -53,8 +65,7 @@ class _GalleryScreenState extends State<GalleryScreen> {
           centerTitle: true,
           backgroundColor: backgroundColor,
         ),
-        body: SingleChildScrollView(
-            child: Padding(
+        body:Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -93,83 +104,62 @@ class _GalleryScreenState extends State<GalleryScreen> {
                       )
                     ],
                   ),
-                  StreamBuilder<List<Reference>>(
-                  stream: photosStream,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      // The photos are available
-                      final photos = snapshot.data;
-                      return ListView.builder(
-                        itemCount: photos?.length,
-                        itemBuilder: (context, index) {
-                          // Display each photo in a Card widget
-                          return Card(
-                            child: ListTile(
-                              leading: FutureBuilder<String>(
-                                future: photos?[index].getDownloadURL(),
-                                builder: (context, snapshot) {
-                                  if (snapshot.hasData) {
-                                    // The download URL is available
-                                    final url = snapshot.data;
-                                    return Image.network(url!);
-                                  } else {
-                                    // The download URL is not available yet
-                                    return CircularProgressIndicator();
-                                  }
-                                },
-                              ),
-                              title: Text(photos![index].name),
-                              trailing: IconButton(
-                                icon: Icon(Icons.delete),
-                                onPressed: () {
-                                  // Delete the photo from Firebase Storage
-                                  photos[index].delete();
-                                },
-                              ),
-                            ),
-                          );
+                   Expanded(child: StreamBuilder<List<String>>(
+                        stream: imageUrlsStream, // a stream of image URLs
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            // the stream has data
+                            final imageUrls = snapshot.data;
+                            return ListView.builder(
+                              itemCount: imageUrls?.length,
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  title: Image.network(imageUrls![index]),
+                                );
+                              },
+                            );
+                          } else {
+                            // the stream has no data yet
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
                         },
-                      );
-                    } else {
-                      // The photos are not available yet
-                      return Center(child: CircularProgressIndicator());
-                    }
-                  })
-
+                      )
+                   )
                 ],
               ),
             )
-        )
     );
   }
 
 
-  void pickMedia(ImageSource source) async {
+  pickMedia(ImageSource source) async {
     XFile? file;
     file = await ImagePicker().pickImage(source: source);
 
     if (file != null) {
       imagePath = file.path;
-      uploadFile();
-      setState(() {});
+      await uploadFile();
     }
   }
 
   Future uploadFile() async {
     if (imagePath == null) return;
     final fileName = basename(File(imagePath as String).path);
-    final destination = 'files/$fileName';
-
-    print(fileName);
+    final uid = await Store.getUser();
+    final destination = '$uid/$fileName';
 
     try {
       final ref = firebase_storage.FirebaseStorage.instance
-          .ref(destination)
-          .child('file/');
+          .ref()
+          .child(destination);
       await ref.putFile(File(imagePath as String));
     } catch (e) {
       print('error occured $e');
     }
+
+    getAllImagesFromStorage();
   }
 
   Widget addButtonStyle(String msg, IconData icon) {
@@ -191,5 +181,38 @@ class _GalleryScreenState extends State<GalleryScreen> {
             ),
           ),*/
       ],);
+  }
+
+  getAllImagesFromStorage() async {
+    final folder = await Store.getUser();
+
+    try {
+      // Reference to the folder in Firebase Storage
+      Reference storageFolder = FirebaseStorage.instance.ref().child(folder!);
+
+      // Retrieve a list of items (files and subdirectories) within the folder
+      ListResult result = await storageFolder.listAll();
+
+      print("result:");
+       List<String> imageUrls = [];
+
+
+
+      // Filter and add only image URLs to the list
+      for (var item in result.items) {
+
+        String imageUrl = await item.getDownloadURL();
+        print(imageUrl);
+        imageUrls.add(imageUrl);
+
+
+      }
+
+      // Add the list of image URLs to the stream
+      imageUrlsController.add(imageUrls);
+    } catch (e) {
+      // Handle errors, if any
+      print('Error getting images from Firebase Storage: $e');
+    }
   }
 }
