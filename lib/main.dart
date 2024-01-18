76d9bbc90da7bc17ded315cce60b09eb405a2f93
Marc Espuña <mespuna@cactusiot.com>
2024-01-18 17:51:51 +0100

    Added Firebase options.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mobils/login.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
