8a7c3efeac3f6492929e569a033d9fbb3e6b4737
marfranco <mfranco@cactusiot.com>
2024-01-21 18:04:42 +0100

    minor change in cte

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF6200EE);
 const primaryVariant = Color(0xFF3700B3);
-const secondaryColor = Color(0xFF03DAC6);
+const secondaryColor = Color(0xFF0347DA);
 const backgroundColor = Color(0xFF121212);
 const surfaceColor = Color(0xEA000000);
 const textColor = Color(0xFFE0E0E0);
